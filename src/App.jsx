80420c82601ee63ddab7c4ce2fb2993af9b2e8ce
Yogesh Verma <avvermabgmi2021@gmail.com>
2026-09-06 10:21:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import './App.css';
 export default function App() {
   const [isLoginView, setIsLoginView] = useState(false);
   const [email, setEmail] = useState('');
